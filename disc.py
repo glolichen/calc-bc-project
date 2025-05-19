0e7269ae3dbe30c037cdb89e59fb07a97c1283b0
glolichen <jlche18659@gmail.com>
2025-05-19 15:36:22 -0400

    asdf

--- disc.py
+++ disc.py
@@ -54,9 +54,9 @@ class Disc(ThreeDScene):
         rects = []
         for i in range(subintervals):
             xi = a + i * dx
-            rect = self.gen_rectangle(axes, xi, 0, xi + dx, function(xi + dx), YELLOW_B)
+            rect = self.gen_rectangle(axes, xi, 0, xi + dx, function(xi + dx), BLUE_E)
             rects.append(rect)
-        return Group(*rects)
+        return rects
 
     def construct(self):
         # text = Tex("Disc Method").scale(2)
@@ -94,78 +94,113 @@ class Disc(ThreeDScene):
         )
 
         # comment to remove rendering solid
-        self.wait(2)
-
-        self.move_camera(phi=75 * DEGREES, theta=30 * DEGREES, zoom=1, run_time=1.5)
-        self.begin_ambient_camera_rotation(rate=0.15)
-
-        surface = Surface(
-            lambda u, v: axes.c2p(
-                v, weirdfunc(v) * np.cos(u), weirdfunc(v) * np.sin(u)
-            ),
-            u_range = [0, 2 * PI],
-            v_range = [0, 5],
-            fill_color = RED
-        )
-        surface.set_fill(RED)
-        self.play(Create(surface))
-        self.wait(2)
-
-        self.move_camera(
-            phi = 0 * DEGREES,
-            theta = -90 * DEGREES, 
-            zoom = 0.8,
-            run_time = 1.5
-        )
-        self.set_camera_orientation(zoom=0.8)
-        self.stop_ambient_camera_rotation()
-
-        self.wait(0.5)
-
-        self.play(Uncreate(surface))
-        self.wait(2)
+        # self.wait(2)
+        #
+        # self.move_camera(phi=75 * DEGREES, theta=30 * DEGREES, zoom=1, run_time=1.5)
+        # self.begin_ambient_camera_rotation(rate=0.15)
+        #
+        # surface = Surface(
+        #     lambda u, v: axes.c2p(
+        #         v, weirdfunc(v) * np.cos(u), weirdfunc(v) * np.sin(u)
+        #     ),
+        #     u_range = [0, 2 * PI],
+        #     v_range = [0, 5],
+        #     fill_color = RED
+        # )
+        # surface.set_fill(RED)
+        # self.play(Create(surface))
+        # self.wait(2)
+        #
+        # self.move_camera(
+        #     phi = 0 * DEGREES,
+        #     theta = -90 * DEGREES, 
+        #     zoom = 0.8,
+        #     run_time = 1.5
+        # )
+        # self.set_camera_orientation(zoom=0.8)
+        # self.stop_ambient_camera_rotation()
+        #
+        # self.wait(0.5)
+        #
+        # self.play(Uncreate(surface))
+        # self.wait(2)
         # end comment to remove rendering solid
-        
 
         integral_text = MathTex("A=\\int_{a}^{b} f(x)\\,\\mathrm dx", color=BLUE)
         integral_text.next_to(axes.coords_to_point(2.5, 0), DOWN, buff=0.2)
+        integral_text.shift(RIGHT)
+        integral_text.shift(RIGHT)
         self.play(Write(integral_text))
 
         riemann_text = MathTex("A\\approx \\sum_{i=1}^{n}f(x_i)\\Delta x", color=YELLOW_B)
         riemann_text.next_to(integral_text, DOWN, buff=0.2)
 
-        riemann = self.gen_riemann(weirdfunc, 0, 5, 5, axes)
-        self.play(FadeIn(riemann), Write(riemann_text))
+        riemann_rects = self.gen_riemann(weirdfunc, 0, 5, 5, axes)
+        particular_rect = riemann_rects[1]
+        particular_rect_under_brace = BraceLabel(particular_rect, r"\Delta x")
+        particular_rect_right_brace = BraceLabel(particular_rect, r"f(x_i)", brace_direction=RIGHT)
+        
+        riemann = Group(*riemann_rects)
+        self.play(
+            FadeIn(riemann),
+            Write(riemann_text)
+        )
+        self.wait(0.5)
+        self.play(
+            Write(particular_rect_under_brace),
+            Write(particular_rect_right_brace)
+        )
 
         # comment to remove riemann animation
         self.wait(1)
         self.remove(riemann)
         for i in range(6, 62, 5):
-            riemann_new = self.gen_riemann(weirdfunc, 0, 5, i, axes)
+            riemann_rects_new = self.gen_riemann(weirdfunc, 0, 5, i, axes)
+            riemann_new = Group(*riemann_rects_new)
             riemann_text_new = MathTex("A\\approx \\sum_{i=1}^{" + str(i) + "}f(x_i)\\Delta x", color=YELLOW_B)
             riemann_text_new.next_to(integral_text, DOWN, buff=0.2)
 
+            particular_rect_new = riemann_rects_new[i // 5]
+            particular_rect_under_brace_new = BraceLabel(particular_rect_new, r"\Delta x")
+            particular_rect_right_brace_new = BraceLabel(particular_rect_new, r"f(x_i)", brace_direction=RIGHT)
+
             self.play(ChangeSpeed(AnimationGroup(
                 Transform(riemann, riemann_new), 
-                Transform(riemann_text, riemann_text_new)
+                Transform(riemann_text, riemann_text_new),
+                Transform(particular_rect_under_brace, particular_rect_under_brace_new),
+                Transform(particular_rect_right_brace, particular_rect_right_brace_new),
             ), speedinfo={0: 2 if i < 25 else 5}, rate_func=linear))
             self.wait(0.1)
 
         riemann_text_new = MathTex("A=\\lim_{n\\to\\infty} \\sum_{i=1}^{n}f(x_i)\\Delta x", color=YELLOW_B)
         riemann_text_new.next_to(integral_text, DOWN, buff=0.2)
-        self.play(Transform(riemann_text, riemann_text_new))
+        self.play(
+            Transform(riemann_text, riemann_text_new),
+            FadeOut(particular_rect_under_brace),
+            FadeOut(particular_rect_right_brace),
+        )
         # end comment to remove riemann animation
 
         self.wait(2)
 
-        riemann_new = self.gen_riemann(weirdfunc, 0, 5, 10, axes)
+        riemann_rects_new = self.gen_riemann(weirdfunc, 0, 5, 10, axes)
+        riemann_new = Group(*riemann_rects_new)
         riemann_text_new = MathTex("A \\approx \\sum_{i=1}^{10}f(x_i)\\Delta x", color=YELLOW_B)
         riemann_text_new.next_to(integral_text, DOWN, buff=0.2)
 
-        self.play(AnimationGroup(
+        particular_rect_new = riemann_rects_new[2]
+        particular_rect_under_brace_new = BraceLabel(particular_rect_new, r"\Delta x")
+        particular_rect_right_brace_new = BraceLabel(particular_rect_new, r"f(x_i)", brace_direction=RIGHT)
+
+        self.play(
             Transform(riemann, riemann_new), 
-            Transform(riemann_text, riemann_text_new)
-        ))
+            Transform(riemann_text, riemann_text_new),
+        )
+        self.wait(0.5)
+        self.play(
+            Write(particular_rect_under_brace_new),
+            Write(particular_rect_right_brace_new),
+        )
 
         self.wait(2)
 
@@ -174,15 +209,29 @@ class Disc(ThreeDScene):
         ), speedinfo = {0: 2}))
         self.move_camera(phi=60 * DEGREES, theta=30 * DEGREES, zoom=1, run_time=1.5)
         self.begin_ambient_camera_rotation(rate=0.25)
-        
+
         # comment to remove discs
         self.wait(0.5)
         cylinders = self.gen_x_cylinder_riemann(weirdfunc, 0, 5, 10, axes)
         self.add(riemann.copy())
         self.play(Transform(riemann, cylinders))
         # end comment to remove discs
-        
-        self.set_camera_orientation(zoom=0.8)
+
+        self.wait(2)
+        self.move_camera(phi=60 * DEGREES, theta=30 * DEGREES + 0.25 * 2, zoom=0.6, run_time=1.5, frame_center=[0, 0, 3])
+            
+        disc_info1 = MathTex("\\text{Cylinder radius}=\\text{Rectangle height}=f(x_i)")
+        disc_info1.scale(0.8)
+        disc_info1.to_edge(UP)
+        self.add_fixed_in_frame_mobjects(disc_info1)
+        self.play(Write(disc_info1))
+
+        disc_info2 = MathTex("\\text{Cylinder height}=\\text{Rectangle width}=\\Delta x")
+        disc_info2.scale(0.8)
+        disc_info2.next_to(disc_info1, DOWN)
+        self.add_fixed_in_frame_mobjects(disc_info2)
+        self.play(Write(disc_info2))
+
         self.wait(4)
 
 
