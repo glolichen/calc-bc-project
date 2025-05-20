bdb86eece0ea9787759d95d474291e6c7275ea1a
glolichen <jlche18659@gmail.com>
2025-05-19 23:17:10 -0400

    finish disc

--- disc.py
+++ disc.py
@@ -59,10 +59,10 @@ class Disc(ThreeDScene):
         return rects
 
     def construct(self):
-        # text = Tex("Disc Method").scale(2)
-        # self.play(Write(text))
-        # self.wait(1)
-        # self.play(Unwrite(text))
+        text = Tex("Disc Method").scale(2)
+        self.play(Write(text))
+        self.wait(1)
+        self.play(Unwrite(text))
 
         axes = ThreeDAxes(
             x_range = [0, 6],
@@ -94,36 +94,36 @@ class Disc(ThreeDScene):
         )
 
         # comment to remove rendering solid
-        # self.wait(2)
-        #
-        # self.move_camera(phi=75 * DEGREES, theta=30 * DEGREES, zoom=1, run_time=1.5)
-        # self.begin_ambient_camera_rotation(rate=0.15)
-        #
-        # surface = Surface(
-        #     lambda u, v: axes.c2p(
-        #         v, weirdfunc(v) * np.cos(u), weirdfunc(v) * np.sin(u)
-        #     ),
-        #     u_range = [0, 2 * PI],
-        #     v_range = [0, 5],
-        #     fill_color = RED
-        # )
-        # surface.set_fill(RED)
-        # self.play(Create(surface))
-        # self.wait(2)
-        #
-        # self.move_camera(
-        #     phi = 0 * DEGREES,
-        #     theta = -90 * DEGREES, 
-        #     zoom = 0.8,
-        #     run_time = 1.5
-        # )
-        # self.set_camera_orientation(zoom=0.8)
-        # self.stop_ambient_camera_rotation()
-        #
-        # self.wait(0.5)
-        #
-        # self.play(Uncreate(surface))
-        # self.wait(2)
+        self.wait(2)
+
+        self.move_camera(phi=75 * DEGREES, theta=30 * DEGREES, zoom=1, run_time=1.5)
+        self.begin_ambient_camera_rotation(rate=0.15)
+
+        surface = Surface(
+            lambda u, v: axes.c2p(
+                v, weirdfunc(v) * np.cos(u), weirdfunc(v) * np.sin(u)
+            ),
+            u_range = [0, 2 * PI],
+            v_range = [0, 5],
+            fill_color = RED
+        )
+        surface.set_fill(RED)
+        self.play(Create(surface))
+        self.wait(2)
+
+        self.move_camera(
+            phi = 0 * DEGREES,
+            theta = -90 * DEGREES, 
+            zoom = 0.8,
+            run_time = 1.5
+        )
+        self.set_camera_orientation(zoom=0.8)
+        self.stop_ambient_camera_rotation()
+
+        self.wait(0.5)
+
+        self.play(Uncreate(surface))
+        self.wait(2)
         # end comment to remove rendering solid
 
         integral_text = MathTex("A=\\int_{a}^{b} f(x)\\,\\mathrm dx", color=BLUE)
@@ -139,7 +139,7 @@ class Disc(ThreeDScene):
         particular_rect = riemann_rects[1]
         particular_rect_under_brace = BraceLabel(particular_rect, r"\Delta x")
         particular_rect_right_brace = BraceLabel(particular_rect, r"f(x_i)", brace_direction=RIGHT)
-        
+
         riemann = Group(*riemann_rects)
         self.play(
             FadeIn(riemann),
@@ -212,92 +212,101 @@ class Disc(ThreeDScene):
 
         # comment to remove discs
         self.wait(0.5)
-        cylinders = self.gen_x_cylinder_riemann(weirdfunc, 0, 5, 10, axes)
-        self.add(riemann.copy())
+        cylinders = self.gen_x_cylinder_riemann(weirdfunc, 0, 5, 2, axes)
+        riemann_copy = riemann.copy()
+        self.add(riemann_copy)
         self.play(Transform(riemann, cylinders))
+
         # end comment to remove discs
 
         self.wait(2)
         self.move_camera(phi=60 * DEGREES, theta=30 * DEGREES + 0.25 * 2, zoom=0.6, run_time=1.5, frame_center=[0, 0, 3])
             
-        disc_info1 = MathTex("\\text{Cylinder radius}=\\text{Rectangle height}=f(x_i)")
+        disc_info1 = MathTex(r"\text{Cylinder radius}=\text{Rectangle height}=f(x_i)")
         disc_info1.scale(0.8)
         disc_info1.to_edge(UP)
         self.add_fixed_in_frame_mobjects(disc_info1)
         self.play(Write(disc_info1))
 
-        disc_info2 = MathTex("\\text{Cylinder height}=\\text{Rectangle width}=\\Delta x")
+        self.wait(2)
+
+        disc_info2 = MathTex(r"\text{Cylinder height}=\text{Rectangle width}=\Delta x")
         disc_info2.scale(0.8)
         disc_info2.next_to(disc_info1, DOWN)
         self.add_fixed_in_frame_mobjects(disc_info2)
         self.play(Write(disc_info2))
 
-        self.wait(4)
+        disc_info3 = MathTex(r"\implies \text{Cylinder volume}=\pi (f(x_i))^2 \Delta x")
+        disc_info3.scale(0.8)
+        disc_info3.next_to(disc_info2, DOWN)
+        self.add_fixed_in_frame_mobjects(disc_info3)
+        self.play(Write(disc_info3))
 
+        self.wait(2)
 
-class DiscText(Scene):
-    def construct(self):
-        # disc_info1 = MathTex("\\text{Cylinder radius}=\\text{Rectangle height}=f(x_i)")
-        # disc_info2 = MathTex("\\text{Cylinder height}=\\text{Rectangle width}=\\Delta x")
-        # disc_info3 = MathTex("\\implies \\text{Cylinder volume}=\\pi (f(x_i))^2 \\Delta x")
-        # disc_info4 = MathTex("\\implies \\sum_{i=1}^{n}\\text{Cylinder volume}=\\sum_{i=1}^{n}\\pi (f(x_i))^2 \\Delta x")
-        # disc_info5 = MathTex("\\implies \\sum_{i=1}^{n}\\text{Cylinder volume}=\\int_{a}^{b} \\pi (f(x))^2\\,\\mathrm dx")
-        # 
-        # disc_info1.move_to([0, 0, 0])
-        # disc_info2.next_to(disc_info1, DOWN, buff=0.2)
-        # disc_info3.next_to(disc_info2, DOWN, buff=0.2)
-        # disc_info4.next_to(disc_info3, DOWN, buff=0.2)
-        # disc_info5.next_to(disc_info4, DOWN, buff=0.2)
-        # 
-        # self.play(
-        #     Write(disc_info1),
-        #     Write(disc_info2),
-        #     Write(disc_info3),
-        #     Write(disc_info4),
-        #     Write(disc_info5),
-        # )
+        disc_info4 = MathTex(r"\implies \text{Volume}\approx \sum_{i=1}^{n} \text{Cylinder volume}")
+        disc_info4.scale(0.8)
+        disc_info4.next_to(disc_info3, DOWN)
+        self.add_fixed_in_frame_mobjects(disc_info4)
+        self.play(Write(disc_info4))
 
-        info1 = MathTex(
-            r"\text{Radius of cylinder }i&=\text{Rectangle height}=f(x_i)=r \\" \
-            r"\text{Height of cylinder }i&=\text{Rectangle width}=\Delta x=h"
+        self.wait(2)
+
+        disc_info4_new = MathTex(r"\implies \text{Volume}\approx \sum_{i=1}^{n} \pi (f(x_i))^2 \Delta x")
+        disc_info4_new.scale(0.8)
+        disc_info4_new.move_to(disc_info4)
+        self.play(Transform(disc_info4, disc_info4_new))
+
+        self.play(
+            FadeOut(disc_info1),
+            FadeOut(disc_info2),
+            FadeOut(disc_info3),
         )
-        info1.move_to([0, 0, 0])
 
-        info2 = MathTex(r"\implies \text{Volume of cylinder }i=\pi r^2 h=\pi (f(x_i))^2 \Delta x")
-        info2.next_to(info1, DOWN, buff=0.2)
+        self.play(
+            disc_info4.animate.to_edge(UP),
+            FadeOut(riemann_copy),
+            FadeOut(particular_rect_under_brace_new),
+            FadeOut(particular_rect_right_brace_new)
+        )
 
-        info3 = MathTex(r"\implies \sum_{i=1}^{n}\text{Volume of cylinder }i=\sum_{i=1}^{n}\pi (f(x_i))^2 \Delta x")
-        info3.next_to(info2, DOWN, buff=0.2)
+        self.wait(2)
 
-        info4 = MathTex(r"\implies \text{Volume of $n$ cylinders}=\sum_{i=1}^{n}\pi (f(x_i))^2 \Delta x")
-        info4.next_to(info2, DOWN, buff=0.2)
+        for i in range(6, 62, 5):
+            cylinders_new = self.gen_x_cylinder_riemann(weirdfunc, 0, 5, i, axes)
+            disc_info4_new = MathTex(r"\text{Volume}\approx \sum_{i=1}^{" + str(i) + r"} \pi (f(x_i))^2 \Delta x")
+            disc_info4_new.scale(0.8)
+            disc_info4_new.move_to(disc_info4)
 
-        info5 = MathTex(r"\implies \lim_{n\to\infty}\text{Volume of $n$ cylinders}=\lim_{n\to\infty}\sum_{i=1}^{n}\pi (f(x_i))^2 \Delta x")
-        info5.next_to(info2, DOWN, buff=0.2)
+            self.play(ChangeSpeed(AnimationGroup(
+                Transform(cylinders, cylinders_new),
+                Transform(disc_info4, disc_info4_new)
+            ), speedinfo={0: 2 if i < 25 else 5}, rate_func=linear))
 
-        info6 = MathTex(r"\implies \text{Volume of solid of revolution}=\lim_{n\to\infty}\sum_{i=1}^{n}\pi (f(x_i))^2 \Delta x")
-        info6.next_to(info2, DOWN, buff=0.2)
+            self.wait(0.2)
 
-        info7 = MathTex(r"\text{Volume of solid of revolution}=\int_{a}^{b}\pi (f(x))^2 \,\mathrm dx")
-        info7.next_to(info2, DOWN, buff=0.2)
-
-        self.play(Write(info1))
-        self.wait(1)
-        self.play(Write(info2))
         self.wait(1)
         
-        self.play(Write(info3))
-        self.wait(3)
-        self.play(Transform(info3, info4))
-        self.wait(3)
-        self.play(Transform(info3, info5))
-        self.wait(3)
-        self.play(Transform(info3, info6))
-        self.wait(3)
-        self.play(Transform(info3, info7))
+        skibidi = MathTex(r"A=\lim_{n\to\infty} \sum_{i=1}^{n}\pi(f(x_i))^2\Delta x")
+        skibidi.to_edge(UP)
+        self.play(Transform(disc_info4, skibidi))
+        
+        self.wait(2)
 
-        box = Rectangle(width=info7.width + 0.5, height=info7.height + 0.5)
-        box.move_to(info7)
-        self.play(ChangeSpeed(Create(box), speedinfo={0: 0.75}))
+        disc_info5 = MathTex(r"\implies \text{Volume}=\int_{a}^{b} \pi (f(x))^2 \mathrm dx", color=YELLOW_B)
+        disc_info5.next_to(disc_info4, DOWN)
+        self.add_fixed_in_frame_mobjects(disc_info5)
+        self.play(Write(disc_info5))
+        
+        self.wait(1)
 
-        self.wait(8)
+        disc_info5_new = MathTex(r"\implies \text{Volume}=\pi\int_{a}^{b} (f(x))^2 \mathrm dx", color=YELLOW_B)
+        disc_info5_new.next_to(disc_info4, DOWN)
+        self.play(Transform(disc_info5, disc_info5_new))
+        
+        formula_box = Rectangle(width=disc_info5.width + 0.25, height=disc_info5.height + 0.25, color=YELLOW_B)
+        formula_box.move_to(disc_info5)
+        self.add_fixed_in_frame_mobjects(formula_box)
+        self.play(ChangeSpeed(Create(formula_box), speedinfo={0: 0.75}))
+        
+        self.wait(4)
